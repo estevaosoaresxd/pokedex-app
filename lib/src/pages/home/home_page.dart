7a444b9f35da7181b7d465c573dec4e95ab4ebc6
Estevão Soares <estevao.silva@4innovation.com.br>
2023-12-26 14:40:14 -0300

    feat: bottom app bar

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,35 +1,49 @@
 import "package:flutter/material.dart";
 import "package:flutter_svg/svg.dart";
 import "package:pokedex_app/src/shared/constants/assets_paths.dart";
+import "package:pokedex_app/src/shared/extensions/context_extension.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ValueNotifier<int> selectedIndex = ValueNotifier<int>(0);
+
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 10,
-        color: Colors.white,
-        child: SizedBox(
-          height: 60,
-          child: Row(
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      AssetsPaths.logoPokemonActived,
-                    ),
-                    const Text("Pokedéx")
-                  ],
-                ),
-              )
-            ],
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: selectedIndex,
+        builder: (context, value, _) => BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: value,
+          onTap: (value) => selectedIndex.value = value,
+          selectedLabelStyle: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
           ),
+          selectedItemColor: context.colorScheme.primary,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(AssetsPaths.logoPokemonDisabled),
+              activeIcon: SvgPicture.asset(AssetsPaths.logoPokemonActived),
+              label: "Pokedéx",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(AssetsPaths.logoLocationDisabled),
+              activeIcon: SvgPicture.asset(AssetsPaths.logoLocationActived),
+              label: "Regiões",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(AssetsPaths.logoFavoritesDisabled),
+              activeIcon: SvgPicture.asset(AssetsPaths.logoFavoritesActived),
+              label: "Favoritos",
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(AssetsPaths.logoProfileDisabled),
+              activeIcon: SvgPicture.asset(AssetsPaths.logoProfileActived),
+              label: "Conta",
+            )
+          ],
         ),
       ),
     );
--- lib/src/shared/constants/assets_paths.dart
+++ lib/src/shared/constants/assets_paths.dart
@@ -5,7 +5,20 @@ abstract class AssetsPaths {
       "lib/src/shared/assets/svg/logo-google.svg";
   static const String logoPokemonActived =
       "lib/src/shared/assets/svg/tab-bar/pokedex.svg";
-
+  static const String logoPokemonDisabled =
+      "lib/src/shared/assets/svg/tab-bar/pokedex-outline.svg";
+  static const String logoLocationActived =
+      "lib/src/shared/assets/svg/tab-bar/location.svg";
+  static const String logoLocationDisabled =
+      "lib/src/shared/assets/svg/tab-bar/location-outline.svg";
+  static const String logoFavoritesActived =
+      "lib/src/shared/assets/svg/tab-bar/favorites.svg";
+  static const String logoFavoritesDisabled =
+      "lib/src/shared/assets/svg/tab-bar/favorites-outline.svg";
+  static const String logoProfileActived =
+      "lib/src/shared/assets/svg/tab-bar/profile.svg";
+  static const String logoProfileDisabled =
+      "lib/src/shared/assets/svg/tab-bar/profile-outline.svg";
   // IMAGES
   static const String littleBoyAndCientistPng =
       "lib/src/shared/assets/images/onboarding_1.png";
